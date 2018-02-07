bc885e0b30c1c296eaa1c5bd43b72f6c7edafc71
Nick Lee <nickoless.lee@gmail.com>
2018-02-07 00:46:16 +0000

    Knex now able to replicate functionality of pg script

--- test-script-knex.js
+++ test-script-knex.js
@@ -1,7 +1,7 @@
 const settings = require("./settings"); // settings.json
 
-let item = process.argv[2]
-const terms = [`%${item}%`]
+const query = `${process.argv[2]}`
+console.log(query)
 
 const knex = require('knex')({
   client: 'pg',
@@ -15,8 +15,24 @@ const knex = require('knex')({
   }
 });
 
+function getData(element) {
+    const id = element.id;
+    const firstName = element.first_name;
+    const lastName = element.last_name;
+    const year = (element.birthdate).getFullYear();
+    const month = (element.birthdate).getMonth();
+    const day = (element.birthdate).getDate();
+  
+    console.log(id + ": " + firstName + " " + lastName + ", " + "born " + "'" + year + "-" + month + "-" + day + "'")
+  };
+
+// knex SQL query
 knex.select('*').from('famous_people')
+.where('first_name', '=', query)
+.orWhereIn('last_name', '=', query)
 .asCallback(function(err, rows) {
   if (err) return console.error(err);
-      console.log(rows);
+
+// function to return data
+    rows.forEach(getData)
     });
